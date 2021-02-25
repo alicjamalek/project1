af3357c330d6b03dd1e752231adf494b902b1a62
Alicja Malek <alicjamalek9@gmail.com>
2021-02-25 19:20:25 +0100

    chyba zrobilam jakas klase

---
+++ src/sample/LeftButton.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+import java.awt.event.ActionEvent;
+
+public class LeftButton {
+
+    @FXML
+    private Button lefButton;
+
+    @FXML
+    void buttonPressed(ActionEvent event) {
+
+    }
+
+}
+
